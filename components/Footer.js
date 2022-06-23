e5f46fb8889b42115ecda79d8fc4ee21bb3320cd
RICO <ricoyou99@gmail.com>
2022-06-23 22:12:55 +0700

    fix error pid map

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,11 @@
+import moment from 'moment';
+
 export default function Footer() {
   return (
     <footer className='flex flex-col items-center justify-between px-4 py-6 mx-auto max-w-7xl md:flex-row bg-prime font-modern xl:max-w-full'>
       <p className='text-sm text-center text-secondary md:text-left md:mb-0 mx-auto'>
-        © Copyright 2022 by Pradita University. All Rights Reserved.
+        © Copyright {moment().format('YYYY')} by Pradita University. All Rights
+        Reserved.
       </p>
     </footer>
   );
